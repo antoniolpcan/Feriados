0928cc6757292688c5f7266e11bc1f687119cc4b
antoniolpcan <antoniolpctwice@gmail.com>
2022-09-24 18:21:16 -0300

    first commit

---
+++ feriados/core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def natal(request):
+    
+    contexto = {'natal': True,
+                'carnaval': False}
+
+    return render(request, 'natal.html', contexto)
