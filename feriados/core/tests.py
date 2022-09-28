b34eee82c3d85b294372d8aabebeb87ff8c74e72
antoniolpcan <antoniolpctwice@gmail.com>
2022-09-28 16:31:59 -0300

    database models

--- feriados/core/tests.py
+++ feriados/core/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from .models import FeriadoModel
+from datetime import datetime
 
 class NatalTest(TestCase):
     
@@ -13,3 +15,26 @@ class NatalTest(TestCase):
 
     def test_template_natal(self):
         self.assertTemplateUsed(self.resp, 'natal.html')
+
+class FeriadoModelTest(TestCase):
+    def setUp(self):
+        self.feriado = "Natal"
+        self.mes = 12
+        self.dia = 25
+        self.cadastro = FeriadoModel(
+                        nome = self.feriado,
+                        dia = self.dia,
+                        mes = self.mes,
+                        )
+        self.cadastro.save()
+
+    def test_created(self):
+        self.assertTrue(FeriadoModel.objects.exists())
+
+    def test_modificado_em(self):
+        self.assertIsInstance(self.cadastro.modificado_em, datetime)
+    
+    def test_nome_feriado(self):
+        nome = self.cadastro.__dict__.get('nome', '')
+        self.assertEqual(nome, self.nome)
+    
--- feriados/core/views.py
+++ feriados/core/views.py
@@ -4,5 +4,5 @@ def natal(request):
     
     contexto = {'natal': True,
                 'carnaval': False}
-
+    # import ipdb ; ipdb.set_trace()
     return render(request, 'natal.html', contexto)
